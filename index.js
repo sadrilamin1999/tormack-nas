8461aa3ab246112c8397421fdb7d7583fa315e89
SadrilAmin1999 <sadrilshuvo420@gmail.com>
2023-06-13 02:12:50 +0600

    device api added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const userRoutes = require("./routes/user");
-
+const deviceRoutes = require("./routes/user");
 // express app
 const app = express();
 app.use(
@@ -20,7 +20,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to Tormack server!" });
 });
+
 app.use("/api/auth/user", userRoutes);
+app.use("/api/data/device", deviceRoutes);
 
 const port = process.env.PORT || 8080;
 const uri = process.env.MONGO_URI;
